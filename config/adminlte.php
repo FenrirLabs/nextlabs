1b88439e997b12964ac0fe5ac491517a2491fcf4
CLOVIS SANTIAGO CAVALHEIRO <clovis.santiago@gmail.com>
2021-11-05 16:30:21 +0000

    Login Blade

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
@@ -46,11 +46,11 @@ return [
     */
 
     'logo' => '<b>Next</b>Labs',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/logo_nextpoint_op2_vertical.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'NextPoint',
 
     /*
     |--------------------------------------------------------------------------
